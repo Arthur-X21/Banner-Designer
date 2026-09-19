23ff5b895336ab1e5bb4389b8f0a0defab8c4975
Arthur-X21 <amineslei02@gmail.com>
2026-09-19 14:06:06 +0100

    Add ScreenInvoker mixin for drawable elements

---
+++ src/main/java/com/bannerdesigner/client/mixin/ScreenInvoker.java
@@ -0,0 +1,15 @@
+package com.bannerdesigner.client.mixin;
+
+import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.Screen;
+import net.minecraft.client.gui.Selectable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(Screen.class)
+public interface ScreenInvoker {
+
+    @Invoker("addDrawableChild")
+    <T extends Element & Drawable & Selectable> T bannerDesigner$addDrawableChild(T drawableElement);
+}
