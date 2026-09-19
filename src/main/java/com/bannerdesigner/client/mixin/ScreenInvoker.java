684669e72ebce94236cac81643f102792186d4bf
Arthur-X21 <amineslei02@gmail.com>
2026-09-19 14:20:55 +0100

    Import Screen class from net.minecraft.client.gui.screen

--- src/main/java/com/bannerdesigner/client/mixin/ScreenInvoker.java
+++ src/main/java/com/bannerdesigner/client/mixin/ScreenInvoker.java
@@ -2,8 +2,8 @@ package com.bannerdesigner.client.mixin;
 
 import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.Element;
-import net.minecraft.client.gui.Screen;
 import net.minecraft.client.gui.Selectable;
+import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
