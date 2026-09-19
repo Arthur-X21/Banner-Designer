2804a6f6c74630d32a1f8a235a076cc4759bb2a5
Arthur-X21 <amineslei02@gmail.com>
2026-09-19 12:43:25 +0100

    Implement BannerDesignerClient for client initialization

---
+++ src/main/java/com/bannerdesigner/client/BannerDesignerClient.java
@@ -0,0 +1,18 @@
+package com.bannerdesigner.client;
+
+import com.bannerdesigner.util.BannerDesignerPaths;
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BannerDesignerClient implements ClientModInitializer {
+    public static final String MOD_ID = "banner-designer";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("Banner Designer initializing (client-side only)...");
+        BannerDesignerPaths.ensureDirectories();
+        LOGGER.info("Banner Designer initialized.");
+    }
+}
