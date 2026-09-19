2bebd98a462aa87ee66739aeca24c1660d430b4d
Arthur-X21 <amineslei02@gmail.com>
2026-09-19 12:45:42 +0100

    Add LoomScreenAccessor mixin interface
    Created a placeholder accessor mixin for LoomScreen.

---
+++ src/main/java/com/bannerdesigner/client/mixin/LoomScreenAccessor.java
@@ -0,0 +1,13 @@
+package com.bannerdesigner.client.mixin;
+
+import net.minecraft.client.gui.screen.ingame.LoomScreen;
+import org.spongepowered.asm.mixin.Mixin;
+
+/**
+ * Placeholder Accessor Mixin. Actual accessors will be added in Phase 8/11
+ * after verifying Yarn field names for 1.21.11 on mappings.dev.
+ */
+@Mixin(LoomScreen.class)
+public interface LoomScreenAccessor {
+    // Intentionally empty for now.
+}
